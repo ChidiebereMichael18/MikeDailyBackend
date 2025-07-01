8eb17365be06085ec10588758ae99785c7483c5a
codewithmike-jpg <mikeyxofficial1234@gmail.com>
2025-07-01 18:26:53 +0100

    .env

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { createUser, updateUser, loginUser, getAllUsers, deleteUser } = require('../controllers/userController');
 
-router.post('/create', createUser);              // POST /api/users
+router.post('/create', createUser);              // POST/api/users
 router.put('/:userId', updateUser);        // PUT /api/users/:userId
 router.post('/login', loginUser);          // POST /api/users/login
 router.get('/', getAllUsers);               // GET /api/users
