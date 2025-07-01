04c4aec7cf280a08c57cbb5e7abcf5878a661339
codewithmike-jpg <mikeyxofficial1234@gmail.com>
2025-07-01 17:27:26 +0100

    updated routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { createUser, updateUser } = require('../controllers/userController');
+const { createUser, updateUser, loginUser } = require('../controllers/userController');
 
 router.post('/', createUser);              // POST /api/users
 router.put('/:userId', updateUser);        // PUT /api/users/:userId
+router.post('/login', loginUser);          // POST /api/users/login
 
 module.exports = router;
